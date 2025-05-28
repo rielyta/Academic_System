4a56ec8a0d3930ae7883ae6dc1addc147f190fd1
rielyta <desi@students.usu.ac.id>
2025-05-28 14:29:50 +0700

    Initial commit

---
+++ src/main/java/com/example/academic_system/AcademicSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.academic_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademicSystemApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/academic_system/models/Mahasiswa.java
@@ -0,0 +1,4 @@
+package com.example.academic_system.models;
+
+public class Mahasiswa {
+}
